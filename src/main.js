8f6d9cd1a20162a0a06c16a421b36dcf010c275a
LandgeRushikesh <rushilandge12@gmail.com>
2025-01-07 16:09:15 +0530

    scroll effect added to about section

--- src/main.js
+++ src/main.js
@@ -1,2 +1,25 @@
 import './style.css'
 
+let paras = document.querySelectorAll(".para")
+
+
+// Scroll effect to about section
+document.addEventListener("scroll", () => {
+    for (let para of paras) {
+        // Get the vertical position of the logo relative to the document
+        let paraTop = para.offsetTop;
+
+        // Get the height of the logo element
+        let paraHeight = para.offsetHeight;
+
+        // Calculate the position of the bottom edge of the viewport
+        let windowBottom = window.scrollY + window.innerHeight;
+
+        // Check if any part of the logo is within the visible viewport
+        if (windowBottom > paraTop && window.scrollY < paraTop + paraHeight) {
+            para.classList.add("visible");
+        } else {
+            para.classList.remove("visible");
+        }
+    }
+});
